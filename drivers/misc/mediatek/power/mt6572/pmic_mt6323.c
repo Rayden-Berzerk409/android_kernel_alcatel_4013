03b720ea0388db5c9a12010ec6915315511cdd29
melekpro <melekpro6@gmail.com>
2024-02-03 00:20:20 +0100

    Add files via upload

--- drivers/misc/mediatek/power/mt6572/pmic_mt6323.c
+++ drivers/misc/mediatek/power/mt6572/pmic_mt6323.c
@@ -20,7 +20,7 @@
 //#include <linux/autoconf.h>
 #include <linux/kernel.h>
 #include <linux/init.h>
-#include <linux/module.h>  
+#include <linux/module.h>
 #include <linux/slab.h>
 #include <linux/sched.h>
 #include <linux/spinlock.h>
@@ -60,7 +60,7 @@
 
 #include <mtk_kpd.h>
 
-#if defined (CONFIG_MTK_KERNEL_POWER_OFF_CHARGING)
+#if defined (MTK_KERNEL_POWER_OFF_CHARGING)
 #include <mach/mt_boot.h>
 #include <mach/system.h>
 #include "mach/mt_gpt.h"
@@ -127,7 +127,7 @@ extern void charger_hv_detect_sw_workaround_init(void);
 extern void pmu_drv_tool_customization_init(void);
 
 
-#if defined (CONFIG_MTK_KERNEL_POWER_OFF_CHARGING)
+#if defined (MTK_KERNEL_POWER_OFF_CHARGING)
 extern void mt_power_off(void);
 static kal_bool long_pwrkey_press = false;
 static unsigned long timer_pre = 0; 
@@ -888,14 +888,15 @@ extern void kpd_pwrkey_pmic_handler(unsigned long pressed);
 void pwrkey_int_handler(void)
 {
     kal_uint32 ret=0;
+#if defined (MTK_KERNEL_POWER_OFF_CHARGING)
 	static kal_bool  key_press = KAL_FALSE;
-
+#endif
     xlog_printk(ANDROID_LOG_INFO, "Power/PMIC", "[pwrkey_int_handler]....\n");
 
     if(upmu_get_pwrkey_deb()==1)
     {
         xlog_printk(ANDROID_LOG_INFO, "Power/PMIC", "[pwrkey_int_handler] Release pwrkey\n");
-#if defined (CONFIG_MTK_KERNEL_POWER_OFF_CHARGING)
+#if defined (MTK_KERNEL_POWER_OFF_CHARGING)
 		if(g_boot_mode == KERNEL_POWER_OFF_CHARGING_BOOT)
 		{
 			if(key_press == KAL_TRUE)
@@ -922,7 +923,7 @@ void pwrkey_int_handler(void)
     else
     {
         xlog_printk(ANDROID_LOG_INFO, "Power/PMIC", "[pwrkey_int_handler] Press pwrkey\n");
-#if defined (CONFIG_MTK_KERNEL_POWER_OFF_CHARGING)
+#if defined (MTK_KERNEL_POWER_OFF_CHARGING)
 		if(g_boot_mode == KERNEL_POWER_OFF_CHARGING_BOOT)
 		{
 			key_press = KAL_TRUE;
@@ -1025,9 +1026,19 @@ void fchr_key_int_handler(void)
     
     ret=pmic_config_interface(INT_STATUS1,0x1,0x1,1);    
 }
+
+
+extern int accdet_irq_handler(void);
+void accdet_int_handler(void)
+{
     kal_uint32 ret=0;
 
     xlog_printk(ANDROID_LOG_INFO, "Power/PMIC", "[accdet_int_handler]....\n");
+    
+   ret = accdet_irq_handler();
+	if(0 == ret){
+		xlog_printk(ANDROID_LOG_INFO, "Power/PMIC", "[accdet_int_handler] don't finished\n");
+	}
     ret=pmic_config_interface(INT_STATUS1,0x1,0x1,2);    
 }
 
@@ -1452,7 +1463,7 @@ ANALDO:
 VTCXO
 VA
 VCAMA
-VCN33 (Â¦Â³wifi/bt Â¶}ÃƒÃ¶)
+VCN33 (¦³wifi/bt ¶}Ãö)
 VCN28
 
 DIGLDO:
@@ -1764,11 +1775,14 @@ void dct_pmic_VCAMD_enable(kal_bool dctEnable)
 
     if(dctEnable == KAL_TRUE)
     {
+	pmic_config_interface(0x55c, 0x1,0x1,0x0);
         upmu_set_rg_vcamd_en(1);
     }
     else
     {
         upmu_set_rg_vcamd_en(0);
+	msleep(20);
+	pmic_config_interface(0x55c, 0x0,0x1,0x0);
     }
 }
 
@@ -3489,7 +3503,7 @@ static int dump_ldo_status_read(char *page, char **start, off_t off,
     p += sprintf(p, "VUSB    =%d\n",  (((val_1)&(0x0400))>>10) );
 
     *start = page + off;
- 
+
     len = p - page;
     if (len > off)
         len -= off;
@@ -3502,7 +3516,6 @@ static int dump_ldo_status_read(char *page, char **start, off_t off,
 
 void pmic_debug_init(void)
 {
-/*
     struct proc_dir_entry *entry;
     struct proc_dir_entry *mt_pmic_dir;
 
@@ -3516,7 +3529,6 @@ void pmic_debug_init(void)
     if (entry) {
         entry->read_proc = dump_ldo_status_read;
     }
-*/
 }
 
 void PMIC_INIT_SETTING_V1(void)
@@ -3634,6 +3646,7 @@ ret = pmic_config_interface(0x778,0x1,0x1,15); // [15:15]: RG_VREF18_ENB_MD;
 
 void PMIC_CUSTOM_SETTING_V1(void)
 {    
+	pmic_config_interface(0x55c, 0x0,0x1,0x0);
 	#ifdef KPD_PMIC_RSTKEY_MAP
 		upmu_set_rg_fchr_keydet_en(0);
 		upmu_set_rg_fchr_pu_en(1);
